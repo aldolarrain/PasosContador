01b18526c3aa2beb3c504eb318e2120f9bef5fc7
aldo larrain <aldo.larrain@hotmail.com>
2021-03-16 18:11:00 -0400

    Agregar archivos de proyecto.

---
+++ ApiColector/Models/Odometro.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiColector.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Odometro
+    {
+        [Key]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime DateTime { get; set; }
+        [Required]
+        public int Step { get; set; }
+
+    }
+
+}
